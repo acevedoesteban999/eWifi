c161e3e65ccd66230484b6a128d468421e1cbac3
Esteban <acevedoesteban999@gmail.com>
2024-12-06 12:31:21 -0500

    UPD: Adapting to v5.2.2

--- eWifi.c
+++ eWifi.c
@@ -1,8 +1,10 @@
 #include "eWifi.h"
 
 
+// Manejador de eventos de WiFi
 void wifi_event_handler(void *arg, esp_event_base_t event_base,int32_t event_id, void *event_data)
 {
+    // Maneja los eventos de conexión y desconexión de estaciones
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
         ESP_LOGI("", "station " MACSTR " join, AID=%d", MAC2STR(event->mac), event->aid);
@@ -13,46 +15,43 @@ void wifi_event_handler(void *arg, esp_event_base_t event_base,int32_t event_id,
     }
 }
 
+// Inicializa el modo SoftAP
 void wifi_init_softap(void)
 {
-    init_nvs();
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    // Registra el manejador de eventos de WiFi
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
 
-    ESP_ERROR_CHECK(
-        esp_event_handler_instance_register(
-            WIFI_EVENT,
-            ESP_EVENT_ANY_ID,
-            &wifi_event_handler,
-            NULL,
-            NULL
-        )
-    );
-
+    // Configuración del AP
     wifi_config_t wifi_config = {
         .ap = {
             .ssid = ESP_WIFI_SSID,
             .ssid_len = strlen(ESP_WIFI_SSID),
             .password = ESP_WIFI_PASS,
             .max_connection = MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK,
-            .pmf_cfg = {
-                    .required = true,
-            },
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
         },
     };
+
+    // Si no hay contraseña, establece el modo abierto
     if (strlen(ESP_WIFI_PASS) == 0) {
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
 
+    // Configura y arranca el WiFi
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_LOGI("", "SSID: %s PASSW:%s",ESP_WIFI_SSID, ESP_WIFI_PASS);
-}
+    // Obtiene la información de IP
+    esp_netif_ip_info_t ip_info;
+    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ip_info);
 
+    char ip_addr[16];
+    inet_ntoa_r(ip_info.ip.addr, ip_addr, 16);
+    ESP_LOGI("", "Set up softAP with IP: %s", ip_addr);
+
+    ESP_LOGI("", "wifi_init_softap finished. SSID:'%s' password:'%s'",
+             ESP_WIFI_SSID, ESP_WIFI_PASS);
+}
--- include/eWifi.h
+++ include/eWifi.h
@@ -4,6 +4,7 @@
 #include "esp_netif.h"
 #include "esp_mac.h"
 #include "eStore.h"
+#include "lwip/inet.h"
 
 #define ESP_WIFI_SSID "ESP32"
 #define ESP_WIFI_PASS "ESP32_PASSW"
