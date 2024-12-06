baedf662ae17f0755650da89b174e5f0c2d73c85
Esteban <acevedoesteban999@gmail.com>
2024-12-06 11:48:27 -0500

    first commit

---
+++ include/eWifi.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "esp_wifi.h"
+#include "esp_netif.h"
+#include "esp_mac.h"
+#include "eStore.h"
+
+#define ESP_WIFI_SSID "ESP32"
+#define ESP_WIFI_PASS "ESP32_PASSW"
+#define MAX_STA_CONN 4
+
+void wifi_init_softap(void);
